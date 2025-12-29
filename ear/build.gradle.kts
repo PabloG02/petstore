aa605a16d62dc0621472e5b6b18efdbfa17a8a32
PabloG02 <devpablogg@gmail.com>
2025-12-29 19:26:04 +0100

    Fix EAR packaging to include WARs instead of JARs

--- ear/build.gradle.kts
+++ ear/build.gradle.kts
@@ -1,12 +1,21 @@
 // EAR module - Enterprise Application Archive packaging
+// TODO: Find a less hacky way to include the WARs in the EAR
 
 plugins {
-    id("ear")
+    ear
 }
 
 dependencies {
-    deploy(project(":jsf"))
-    deploy(project(":rest"))
-    deploy(project(":service"))
+    // Keep domain and service on the classpath inside /lib
+    earlib(project(":service"))
     earlib(project(":domain"))
+}
+
+tasks.named<Ear>("ear") {
+    // Ensure WARs are built first
+    dependsOn(":jsf:war", ":rest:war")
+
+    // Place WARs at the EAR root
+    from(project(":jsf").tasks.named("war"))
+    from(project(":rest").tasks.named("war"))
 }
--- jsf/build.gradle.kts
+++ jsf/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("war")
+    war
 }
 
 dependencies {
